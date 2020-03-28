0fe67a2240c30b6b79b088ec52495820a479ad4d
zhangyaowen <zyw19871007@163.com>
2020-03-28 11:20:40 +0800

    1

--- study.py
+++ study.py
@@ -1,60 +1,60 @@
-
 # coding: utf-8
 
 # In[1]:
 
-import os
 from uiautomator import device as driver
 import numpy as np
 import time
 import os
+import random
 import sys
-
 
 # In[2]:
 
 
-Height=1280
-Width=720
-all_of_list=[]
+Height = 1280
+Width = 720
+all_of_list = []
 if os.path.isfile("db.npy"):
-    all_of_list = np.load ("db.npy").tolist()
+    all_of_list = np.load("db.npy").tolist()
+
 
 # In[3]:
 
 
-def autoJob(tv,sleep_time,sum=6,click=True):
-    count_click=0
-    count=0
-    drag_str='adb shell input swipe '+str(Width*0.5)+' '+str(Height*0.88)+' '+str(Width*0.5)+' '+str(Height*0.3)
+def autoJob(tv, sleep_time, sum=6, click=True):
+    count_click = 0
+    count = 0
+    drag_str = 'adb shell input swipe ' + str(Width * 0.5) + ' ' + str(Height * 0.88) + ' ' + str(
+        Width * 0.5) + ' ' + str(Height * 0.3)
     for _ in range(100):
-        text_lists=driver(className='android.widget.TextView')
+        text_lists = driver(className='android.widget.TextView')
         try:
             for i in range(len(text_lists)):
-                txt=text_lists[i].text
-                if len(txt)>11 and txt not in all_of_list and count<sum:
-                    driver(text=txt,className='android.widget.TextView').click()
-                    #分享，收藏，评论
-                    if click and count_click<2:
-                        #分享
+                txt = text_lists[i].text
+                if len(txt) > 11 and txt not in all_of_list and count < sum:
+                    driver(text=txt, className='android.widget.TextView').click()
+                    # 分享，收藏，评论
+                    if click and count_click < 2:
+                        # 分享
                         time.sleep(4)
-                        driver.click(0.94*Width, 0.975*Height)
+                        driver.click(0.94 * Width, 0.975 * Height)
                         time.sleep(2)
                         driver(text="分享到学习强国").click()
                         time.sleep(2)
                         driver.press.back()
-                        #收藏
-                        driver.click(0.84*Width, 0.975*Height)
-                        #评论
+                        # 收藏
+                        driver.click(0.84 * Width, 0.975 * Height)
+                        # 评论
                         time.sleep(1)
                         driver(text="欢迎发表你的观点").click()
                         time.sleep(2)
                         # os.system("adb shell am broadcast -a ADB_INPUT_TEXT --es msg '中国加油，武汉加油！'")
                         os.system("adb shell input text 'China Comes Wuhan Comes'")
-                        os.system("adb shell input keyevent 66")#不知道为什么输入一个回车，点击发布才有反应
+                        os.system("adb shell input keyevent 66")  # 不知道为什么输入一个回车，点击发布才有反应
                         time.sleep(2)
                         driver(text="发布").click()
-                        count_click=count_click+1
+                        count_click = count_click + 1
                         '''
                         @liuzhijie443
                         #收藏
@@ -68,14 +68,18 @@ def autoJob(tv,sleep_time,sum=6,click=True):
                         time.sleep(2)
                         driver(text="确认").click()
                         '''
-                    count=count+1
+                    count = count + 1
                     all_of_list.append(txt)
-                    print("正在"+tv+"...",txt)
-                    time.sleep(sleep_time)
+                    print("正在" + tv + "...", txt)
+                    if click:
+                        random_sleep(sleep_time)
+                    else:
+                        time.sleep(sleep_time)
+                        time.sleep(random.randint(0, 20))
                     driver.press.back()
         except BaseException:
             print("抛出异常，程序继续执行...")
-        if count >=sum:
+        if count >= sum:
             break
         os.system(drag_str)
 
@@ -88,45 +92,47 @@ def watch_local():
     time.sleep(30)
     print("本地频道结束")
     driver.press.back()
+
+
 # In[4]:
 
 
-#阅读文章,阅读6个文章，每个文章停留130秒
+# 阅读文章,阅读6个文章，每个文章停留130秒
 def read_articles():
     time.sleep(2)
-    #切换到要闻界面
+    # 切换到要闻界面
     driver(text='新思想').click()
-    autoJob(tv="阅读文章",sleep_time=130)
+    autoJob(tv="阅读文章", sleep_time=130)
     print("阅读文章结束")
 
 
 # In[5]:
 
 
-#观看视频,每个视频观看20秒，以及17分钟新闻联盟
+# 观看视频,每个视频观看20秒，以及17分钟新闻联盟
 def watch_video():
     time.sleep(2)
-    #切换到电视台页面
+    # 切换到电视台页面
     driver(resourceId="cn.xuexi.android:id/home_bottom_tab_button_contact").click()
     driver(text="联播频道").click()
-    autoJob(tv="观看视频",sleep_time=20,click=False)
+    autoJob(tv="观看视频", sleep_time=20, click=False)
     driver(text="联播频道").click()
-    
-    news=None
+
+    news = None
     for v in driver(className='android.widget.TextView'):
         if "《新闻联播》" in v.text:
-            news=v.text
+            news = v.text
             break
     driver(text=news).click()
 
-    #100天后删除最早一天的记录
-    text_list=np.array (all_of_list)
-    
-    if len(text_list)>2500:
+    # 100天后删除最早一天的记录
+    text_list = np.array(all_of_list)
+
+    if len(text_list) > 2500:
         text_list = text_list[25:]
-    #存储已看视频和文章
-    np.save ('db.npy',text_list)
-    
+    # 存储已看视频和文章
+    np.save('db.npy', text_list)
+
     print("正在观看新闻联播...")
     time.sleep(1050)
     driver.press('back')
@@ -135,25 +141,40 @@ def watch_video():
 
 # In[6]:
 
+
+def random_sleep(sleep_time):
+    # 休息时，滑动滚轮
+    print(sleep_time)
+    y = Height
+    j = int(sleep_time / 20)
+    for i in range(j):
+        driver.swipe(0, 8 / 10 * y, 0, 2 / 10 * y, 50 + random.randint(0, 50))
+        time.sleep(20 + random.randint(0, 9))
+
+
+# In[7]:
+
+
 if __name__ == '__main__':
-    #自动打开学习强国
-    #os.system('adb shell am start cn.xuexi.android/com.alibaba.android.rimet.biz.SplashActivity')
+    # 自动打开学习强国
+    # os.system('adb shell am start cn.xuexi.android/com.alibaba.android.rimet.biz.SplashActivity')
     os.system('adb version')
     os.system('adb kill-server')
     os.system('adb connect 127.0.0.1:7555')
     time.sleep(2)
     print("init")
-    #屏幕高度
-    Height=driver.info['displayHeight']
-    Width=driver.info['displayWidth']
-    print("Height "+"{}".format(Height)+"width"+"{}".format(Width))
-    #切换adb输入法
+    # 屏幕高度
+    Height = driver.info['displayHeight']
+    Width = driver.info['displayWidth']
+    print("Height " + "{}".format(Height) + "width" + "{}".format(Width))
+    # 切换adb输入法
     # os.system('adb shell ime set com.android.adbkeyboard/.AdbIME')
     # start
     watch_local()
     read_articles()
     watch_video()
-    #切换回搜狗输入法
-    #os.system('adb shell ime set com.sohu.inputmethod.sogou.meizu/com.sohu.inputmethod.sogou.SogouIME')
-    #熄灭屏幕
+    # random_sleep(100)
+    # 切换回搜狗输入法
+    # os.system('adb shell ime set com.sohu.inputmethod.sogou.meizu/com.sohu.inputmethod.sogou.SogouIME')
+    # 熄灭屏幕
     os.system('adb shell input keyevent 26')
